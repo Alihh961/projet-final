1221c0c800f175a5b0eb76aaa8519e2666b7ae4d
Alihh961 <hajhassan.ali@outlook.com>
2022-10-27 04:19:03 +0200

    modification pagination

--- script/java.js
+++ script/java.js
@@ -6,8 +6,19 @@
 
 var pageActuelle = 1;
 var proAffi = 10; // Nombre(max) des produits qui seront affichés
-var produits = document.querySelectorAll('.product');
-var produitsContainer = document.getElementById('produits-list');
+var produits = document.querySelectorAll('.product');// toute les produits de shop page
+var produitsContainer = document.getElementById('produits-list'); // le conteneur qui contient les produits qui seront affichés
+
+
+
+// Calculer le nombre de pages nécessaires pour afficher tous les produits
+
+function numPages()
+{
+    return Math.ceil(produits.length / proAffi); // 4 pages
+};
+
+// Arrows control selon la page actuelle
 
 function prevProduits()
 {
@@ -24,6 +35,9 @@ function nextProduits()
         changePage(pageActuelle);
     }
 }
+
+
+// Changment de produits
     
 function changePage(page)
 {
@@ -32,35 +46,35 @@ function changePage(page)
     var produitsContainer = document.getElementById('produits-list');
     var page_span = document.getElementById("page");
 
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
+    produitsContainer.innerHTML = ""; // vider le conteneur sinon les prev produits vont rester lorsque nous afficherons plus de produits
 
-    produitsContainer.innerHTML = "";
-
+    // afficher 10 produits à partir de l'index 0 
+    
     for (var i = (page-1) * proAffi; i < (page * proAffi) && i < produits.length; i++) {
-     
-      produitsContainer.append(produits[i]) + "<br>";
+      produitsContainer.append(produits[i]);
     }
     page_span.innerHTML = page + "/" + numPages();
 
     if (page == 1) {
-        prev.style.opacity ='0.5'
+        prev.style.opacity ='0.5';
+        prev.style.cursor ='no-drop';
     } else {
-        prev.style.opacity ='1'
+        prev.style.opacity ='1';
+        prev.style.cursor ='auto';
+
     }
 
     if (page == numPages()) {
-        next.style.opacity ='0.5'
+        next.style.opacity ='0.5';
+        next.style.cursor ='no-drop';
     } else {
-        next.style.opacity ='1'
+        next.style.opacity ='1';
+        next.style.cursor ='auto';
     }
 }
 
-function numPages()
-{
-    return Math.ceil(produits.length / proAffi);
-}
 
+// Affichant les 10 premiers produits
 window.onload = function() {
     changePage(1);
 };
