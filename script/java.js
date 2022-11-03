a3bf3205d2200feb84d6abb4cdb412be50d23992
Alihh961 <hajhassan.ali@outlook.com>
2022-11-03 13:29:06 +0100

    slider test

--- script/java.js
+++ script/java.js
@@ -6,7 +6,7 @@ console.log(titlePage.item(0).innerHTML);
 
 //******************** Slider dans la page d'accueil  ****************************//
 
-if(titlePage.innerHTML.indexOf('swipper') !== 1 ) {
+if(titlePage.innerHTML.indexOf('Wika') !== 1 ) {
 
  var swiper = new Swiper(".mySwiper",  {
     spaceBetween: 30,
